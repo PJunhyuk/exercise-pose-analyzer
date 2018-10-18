ba01bb2a6a78eefc74e7f5956e1bb66ce0f7b08e
Junhyuk Park <jgravity@naver.com>
2018-10-19 01:27:40 +0900

    Update download_testset_wget.sh

--- testset/download_testset_wget.sh
+++ testset/download_testset_wget.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget "http://flovill.com/dataset/shoulder_press_1.mp4"
-wget "http://flovill.com/dataset/shoulder_press_2.mp4"
-wget "http://flovill.com/dataset/shoulder_press_3.mp4"
+wget "http://jgravity0.cafe24.com/dataset/shoulder_press_1.mp4"
+wget "http://jgravity0.cafe24.com/dataset/shoulder_press_2.mp4"
+wget "http://jgravity0.cafe24.com/dataset/shoulder_press_3.mp4"
