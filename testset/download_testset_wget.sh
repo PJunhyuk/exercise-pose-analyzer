a2793ede6b150e5ae20185e14f8b4ad3a08f4196
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-11-12 15:49:38 +0900

    Update links in download_testset_wget.sh

--- testset/download_testset_wget.sh
+++ testset/download_testset_wget.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget "http://jgravity0.cafe24.com/dataset/shoulder_press_1.mp4"
-wget "http://jgravity0.cafe24.com/dataset/shoulder_press_2.mp4"
-wget "http://jgravity0.cafe24.com/dataset/shoulder_press_3.mp4"
+wget "http://jgravity2.cafe24.com/dataset_public/exercise-pose-analyzer/shoulder_press_1.mp4"
+wget "http://jgravity2.cafe24.com/dataset_public/exercise-pose-analyzer/shoulder_press_2.mp4"
+wget "http://jgravity2.cafe24.com/dataset_public/exercise-pose-analyzer/shoulder_press_3.mp4"
